da923e57d77cce38335951dfc28d4f9782e616d1
Liujiagang8899 <81350685+Liujiagang8899@users.noreply.github.com>
2022-02-28 22:49:07 +0800

    spring cloud
    spring cloud

--- src/main/java/SpringTest.java
+++ src/main/java/SpringTest.java
@@ -2,5 +2,6 @@ public class SpringTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Spring boot");
+         System.out.println("Hello Spring cloud");
     }
 }
