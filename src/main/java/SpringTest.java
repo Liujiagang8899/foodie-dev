03229bf26ea21823508803b5c7c87f63adeea59e
Liujiagang8899 <2728491312@qq.com>
2022-02-28 22:51:20 +0800

    spring 学习之路

--- src/main/java/SpringTest.java
+++ src/main/java/SpringTest.java
@@ -2,6 +2,7 @@ public class SpringTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Spring boot");
-         System.out.println("Hello Spring cloud");
+        System.out.println("Hello Spring cloud");
+        System.out.println("spring 学习之旅");
     }
 }
